92681949d5f7319becc26516feaefe90d0ddf588
Adam <adam@Adams-MacBook-Air.local>
2019-03-20 22:04:03 +1000

    pagination and single post file

--- index.php
+++ index.php
@@ -7,10 +7,17 @@
     <?php 
     if ( have_posts() ) : while ( have_posts() ) : the_post();
   
-      get_template_part( 'content', get_post_format() );
+        get_template_part( 'content', get_post_format() );
+      endwhile; ?>
 
-    endwhile; endif; 
-    ?>
+      <nav>
+        <ul class="pager">
+          <li><?php next_posts_link( 'Previous' ); ?></li>
+          <li><?php previous_posts_link( 'Next' ); ?></li>
+        </ul>
+      </nav>
+    
+    <?php endif; ?>
   </div><!-- /.blog-main -->
 
   <?php get_sidebar(); ?>
